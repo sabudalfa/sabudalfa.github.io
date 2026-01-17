d3d13cda285205c9ec3ea680c11ead7b111f5275
sabudalfa <sabudalfa@users.noreply.github.com>
2026-01-17 12:54:55 +0000

    [ci] deploy site 8798cd35c59675af0a2fc731a3e1ddec445d6a27

--- assets/use-pagetitle-CFeFh4c1.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-Di5hcZN0.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
---
+++ assets/use-pagetitle-DHozOo8V.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-CSjlci_k.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
