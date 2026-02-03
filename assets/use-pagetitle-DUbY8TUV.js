7ff3ac0ed0866a95460e9081e0d16463b20a3bdb
sabudalfa <sabudalfa@users.noreply.github.com>
2026-02-03 06:02:24 +0000

    [ci] deploy site 37f52a87f6fd91df81d1b711162f030dfe61b7f7

--- assets/use-pagetitle-DUbY8TUV.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-DrKtr0Kb.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
---
+++ assets/use-pagetitle-yB8e3L3f.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-1PSXNg_q.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
